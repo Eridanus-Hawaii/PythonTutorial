35a611ec8a55138c76b3471c65b63fb5260d357e
Hana Suzuki <94371110+blume87@users.noreply.github.com>
2022-12-16 16:10:46 +0900

    loop

---
+++ Chapter_3/loop.py
@@ -0,0 +1,24 @@
+from prometheus_client import Counter
+
+
+from collections import Counter
+
+name = "yamada"
+print(name)
+
+name_map = Counter(name)
+print(name_map)
+print('------------------')
+
+str = ''
+for letter in name_map:
+    n = name_map[letter]
+
+    for i in range(n):
+        print(letter)
+        str += letter
+
+    print('---')
+
+print('------------------')
+print(str)
