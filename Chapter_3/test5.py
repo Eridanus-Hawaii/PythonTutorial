1c82c1d77637451656868377ac2ba3beb42525ea
Ryos Suzuki <ryos@sinby.com>
2022-12-16 16:06:42 +0900

    add loop.py

--- Chapter_3/test5.py
+++ Chapter_3/test5.py
@@ -2,10 +2,35 @@ import sys
 from collections import Counter
 from my_utils import counter_to_string
 
-if __name__ == '__main__':
+# additional practice on 12/15/22
+name = 'yamada'
+print(name)
+
+name_map = Counter(name)
+print(name_map)
+print('----------------')
+
+
+str = ''
+for letter in name_map:
+    n = name_map[letter] # number of each letter in Counter  
+
+    for i in range(n):
+        #print(letter)
+        str += letter
+    #print(n)
+
+    print('---')
+
+print('----------------')
+print(str)
+
+
+
+"""if __name__ == '__main__':
     target_name = sys.argv[1]
 
     target_name_map = Counter(target_name)
 
     result = counter_to_string(target_name_map)
-    print(target_name_map, " => ", result)
+    print(target_name_map, " => ", result)"""
