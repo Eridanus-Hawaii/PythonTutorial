2259a9447e774940a7036d57d4e79347f44366e2
AnnaGG4strn0m3r <annag7@hawaii.edu>
2023-01-19 20:53:10 -1000

    process_choice_anna.py

---
+++ Chapter_3/process_choice_anna.py
@@ -0,0 +1,24 @@
+import sys
+def process_choice(name):
+    while True:
+        choice = input('\n単語入れてね。エンターしたら終了。# でも終了:')
+        if choice == '':
+            sys.exit()
+        elif choice == '#':
+            sys.exit()
+        else:
+            candidate = ''.join(choice.lower().split())
+            
+        choice = candidate
+        result = process_choice_pure(name, choice)
+        return result
+
+
+def process_choice_pure(target_name, choice_name):
+    print('本当はアナグラム', target_name, 'から', choice_name, 'を探す')
+    return target_name
+
+if __name__ == '__main__':
+    name = sys.argv[1]
+    phrase = process_choice_pure(name)
+    print("process_choice, name:", phrase)
