485c25e05f9c8580ee2b4afa866f898c074104c5
Ryos Suzuki <ryos@sinby.com>
2023-03-23 15:55:17 +0900

    update test1_ryos.py

--- Chapter_4/test1_ryos.py
+++ Chapter_4/test1_ryos.py
@@ -1,10 +1,26 @@
-text = ['REST', 'TRANSPORT', 'YOU', 'GODWIN', 'VILLAGE', 'ROANOKE', 'WITH', 'ARE', 'YOUR', 'IS', 'JUST', 'SUPPLIES', 'FREE', 'SNOW', 'HEADING', 'TO', 'GONE', 'TO', 'SOUTH', 'FILLER']
+cypher_text = ['REST', 'TRANSPORT', 'YOU', 'GODWIN', 'VILLAGE', 'ROANOKE', 'WITH', 'ARE', 'YOUR', 'IS', 'JUST', 'SUPPLIES', 'FREE', 'SNOW', 'HEADING', 'TO', 'GONE', 'TO', 'SOUTH', 'FILLER']
 
-honto_text = [None] * 20
+honto_text = []
+for i in range(20):
+    honto_text.append(i)
+
 order = [16, 12, 8, 4, 0, 1, 5, 9, 13, 17, 18, 14, 10, 6, 2, 3, 7, 11, 15, 19]
 
+def my_view(lst):
+    i = 0
+    for ii in range(5):
+        for jj in range(4):
+            print(f'{lst[i]} ', end=' ')
+            i += 1
+        print()
+
+import time
+
 for i, e  in enumerate(order):
-    honto_text[e] = text[i]
+    honto_text[e] = cypher_text[i]
+    my_view(honto_text)
+    print("============")
+    time.sleep(1)
 
 print(honto_text)
 
