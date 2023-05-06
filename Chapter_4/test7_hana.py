7375f6de9b072d3b0e71eb511b436f6c25cf2332
Hana Suzuki <94371110+blume87@users.noreply.github.com>
2023-05-06 15:35:23 +0900

    追加

---
+++ Chapter_4/test7_hana.py
@@ -0,0 +1,17 @@
+def to_start_pos(x):
+    if x == 1:
+        return 0
+    elif x == 2:
+        return 1
+    elif x == 3:
+        return 2
+    elif x == 4:
+        return 3
+    elif x == -1:
+        return -16
+    elif x == -2:
+        return -17
+    elif x == -3:
+        return -18
+    elif x == -4:
+        return -19
